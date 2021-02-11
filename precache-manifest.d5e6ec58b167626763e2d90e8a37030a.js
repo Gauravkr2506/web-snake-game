674c3ce3d787c3f3b9117f798f2227144edbffec
gauravkr2506 <gauravkr2506@gmail.com>
2021-02-11 20:17:59 +0530

    Updates

---
+++ precache-manifest.d5e6ec58b167626763e2d90e8a37030a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4293d97c4918a7f0df5998369bac76fe",
+    "url": "/web-snake-game/index.html"
+  },
+  {
+    "revision": "fb4cd2edc659f9a56267",
+    "url": "/web-snake-game/static/css/main.709b2b0b.chunk.css"
+  },
+  {
+    "revision": "89ecda28ca8aab6bdede",
+    "url": "/web-snake-game/static/js/2.8bb3fec1.chunk.js"
+  },
+  {
+    "revision": "4150a627f5138970747231fcef63f425",
+    "url": "/web-snake-game/static/js/2.8bb3fec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb4cd2edc659f9a56267",
+    "url": "/web-snake-game/static/js/main.615eb574.chunk.js"
+  },
+  {
+    "revision": "699f8c05f300d2655f7c",
+    "url": "/web-snake-game/static/js/runtime-main.0e96e01a.js"
+  }
+]);
